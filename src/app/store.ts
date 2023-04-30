778d509ce0295585f9474341cb0da919d1bca260
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2023-04-30 11:03:35 -0600

    17,Redux Toolkit,17.30

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
+import {Action, AnyAction, applyMiddleware, combineReducers, legacy_createStore, Middleware} from 'redux'
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import thunkMiddleware, {ThunkDispatch} from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from "../features/Login/auth-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 
 // объединяя reducer-ы с помощью combineReducers,
@@ -15,8 +16,15 @@ const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer
 })
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware,)
+})
+
+
 // непосредственно создаём store
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
+//export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 // создаем тип диспатча который принимает как AC так и TC
